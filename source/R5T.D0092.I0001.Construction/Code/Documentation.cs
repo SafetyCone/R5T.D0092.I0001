aca8fef1be1b565e488dd8e996d89f12a5daca8e
davidcoats <david@getsafetycone.com>
2021-11-09 16:32:38 -0800

    Initial commit of source-stub.

---
+++ source/R5T.D0092.I0001.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.D0092.I0001.Construction
+{
+    /// <summary>
+    /// Construction project for Zero-dependency ServiceProviderStartupBase providing basic, common, services..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.D0092.I0001/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.D0092.I0001
+{
+    /// <summary>
+    /// Zero-dependency ServiceProviderStartupBase providing basic, common, services.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
