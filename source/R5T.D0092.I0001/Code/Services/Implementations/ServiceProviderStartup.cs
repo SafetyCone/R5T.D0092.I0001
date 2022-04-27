aaa4c88fff2b63aa67a4fc301ed35739632ecb61
davidcoats <david@getsafetycone.com>
2022-04-27 15:28:33 -0700

    Interim changes.

--- source/R5T.D0092.I0001/Code/Services/Implementations/ServiceProviderStartup.cs
+++ source/R5T.D0092.I0001/Code/Services/Implementations/ServiceProviderStartup.cs
@@ -1,15 +1,15 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-using R5T.A0002;
+using R5T.A0002;using R5T.T0064;
 
 
 namespace R5T.D0092.I0001
-{
-    public class ServiceProviderStartup : IServiceProviderStartup
+{[ServiceImplementationMarker]
+    public class ServiceProviderStartup : IServiceProviderStartup,IServiceImplementation
     {
         public Task ConfigureConfiguration(IConfigurationBuilder configurationBuilder)
         {
